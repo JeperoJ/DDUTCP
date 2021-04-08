302e2bd25429ced672d8d6dd292a1e46f87c903a
JeperoJ <jeppejensen009@gmail.com>
2021-04-08 15:06:02 +0200

    full ui, connection needs fixing

--- TCPclient/MainWindow.xaml.cs
+++ TCPclient/MainWindow.xaml.cs
@@ -27,39 +27,101 @@ namespace TCPclient
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+                
+            }
+            catch { }
+
+            /*while (true)
+            {
+                
+            }*/
         }
 
-        public TcpClient client = new TcpClient();
+        private static TcpClient client = new TcpClient();
+        private static NetworkStream stream;
+        private static Thread thr;
 
         void Connection(object sender, RoutedEventArgs e)
         {
             try
             {
                 client.Connect(ip.Text, Convert.ToInt32(port.Text));
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                stream = client.GetStream();
+
+                thr = new Thread(new ThreadStart(Receive));
+                thr.Start();
+
+
             }
             catch //(SocketException excep)
             {
                 //new ToastContentBuilder().AddArgument("action", "viewConversation").AddArgument("conversationId", 9813).AddText(Convert.ToString(excep)).Show();
 
             }
+            finally
+            {
+                connect.Content = "Disconnect";
+                connect.Click += Disconnect;
+            }
+        }
+
+        void Disconnect(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                stream.Close();
+                client.Close();
+            }
+            catch //(SocketException excep)
+            {
+                //new ToastContentBuilder().AddArgument("action", "viewConversation").AddArgument("conversationId", 9813).AddText(Convert.ToString(excep)).Show();
+
+            }
+            finally
+            {
+                connect.Content = "Connect";
+                connect.Click += Connection;
+            }
         }
 
         void Send(object sender, RoutedEventArgs e)
         {
+            //thr.Abort();
+            
             // Translate the passed message into ASCII and store it as a Byte array.
             Byte[] data = Encoding.ASCII.GetBytes(sendMessage.Text);
-
-            // Get a client stream for reading and writing.
-            //  Stream stream = client.GetStream();
-
-            NetworkStream stream = client.GetStream();
-
             // Send the message to the connected TcpServer.
             stream.Write(data, 0, data.Length);
 
             sentMessage.Content = sendMessage.Text;
 
             sendMessage.Text = "";
+            //thr.Start();
+        }
+
+        void Receive()
+        {
+            //MainWindow window = new MainWindow();
+            // Receive the TcpServer.response.
+
+            // Buffer to store the response bytes.
+            Byte[] data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = Encoding.ASCII.GetString(data, 0, bytes);
+            receiveMessage.Content = responseData;
+           
+            
         }
     }
 }
