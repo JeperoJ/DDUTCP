12b4d83d074d76ef0ceb71c657a2c727823c024c
JeperoJ <jeppejensen009@gmail.com>
2021-04-21 08:23:42 +0200

    Start of RSA

--- TCPclient/MainWindow.xaml.cs
+++ TCPclient/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using System.Threading;
 using System.Net.Sockets;
 using System.Net;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace TCPclient
 {
@@ -23,28 +25,38 @@ namespace TCPclient
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            this.Title = "Client Connect";
-            /*try
-            {
-                
-            }
-            catch { }*/
-
-            // Receive the TcpServer.response.
-
-
-        }
-
         //Definerer variabler der skal være globale
         private static TcpClient client = new TcpClient(); //Klienten der håndterer at oprette forbindelse
         private static NetworkStream stream; //Streamen hvor informationen er
         //private static Thread thr; //Threaden hvor receive er
         private static Boolean open;
         private static CancellationTokenSource source;
+        private static RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
         //private static ManualResetEvent mre = new ManualResetEvent(false); //Event til at stoppe receive funktionen
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Client Connect";
+            RSAParameters RSAParam = RSA.ExportParameters(true);
+            stackpanel.Children.Add(new TextBlock { Foreground = sentMessage.Foreground, FontSize = sentMessage.FontSize, Margin = sentMessage.Margin, Background = sentMessage.Background, TextWrapping = sentMessage.TextWrapping,  Text = Convert.ToBase64String(RSAParam.D) + " ;;;;;;;;; " + Convert.ToBase64String(RSAParam.Modulus) });
+            string fileName = @"C:\Users\jeppe\Desktop\DDU Projekt\penis.txt";
+            // Check if file already exists. If yes, delete it.
+
+            // Create a new file     
+            using (FileStream fs = File.Create(fileName))
+            {
+                // Add some text to file    
+                Byte[] title = new UTF8Encoding(true).GetBytes(Convert.ToBase64String(RSAParam.D) + " ;;;;;;;;; " + Convert.ToBase64String(RSAParam.Modulus));
+                fs.Write(title, 0, title.Length);
+                byte[] author = new UTF8Encoding(true).GetBytes("\n\n" + String.Join(",", RSAParam.D.Select(p => p.ToString())));
+                fs.Write(author, 0, author.Length);
+            }
+
+
+
+        }
+
+        
 
         void Connection(object sender, RoutedEventArgs e)
         {
@@ -62,6 +74,10 @@ namespace TCPclient
                 connect.Content = "Disconnect";
                 connect.Click -= Connection;
                 connect.Click += Disconnect;
+
+                /*RSAParameters RSAParam = RSA.ExportParameters(true);
+                Byte[] data = Encoding.UTF8.GetBytes(Convert.ToBase64String(RSAParam.D) + " ;;;;;;;;; " + Convert.ToBase64String(RSAParam.Modulus));
+                stream.Write(data, 0, data.Length);*/
 
             }
             catch (FormatException ex) //Fanger format fejl for input
@@ -113,11 +129,35 @@ namespace TCPclient
             //mre.Reset(); //Slukker for receive threaden
             
             // Translate the passed message into ASCII and store it as a Byte array.
-            Byte[] data = Encoding.UTF8.GetBytes(sendMessage.Text); //Konverterer beskeden til bytes med UTF8 karakter sættet
+            //Byte[] data = Encoding.UTF8.GetBytes(sendMessage.Text); //Konverterer beskeden til bytes med UTF8 karakter sættet
+            Byte[] encryptedData = { };
+            try
+            {
+                encryptedData = RSAEncrypt(Encoding.UTF8.GetBytes(sendMessage.Text), RSA.ExportParameters(false), false);
+            }
+            catch (ArgumentNullException ex)
+            {
+                MessageBox.Show("Enkryption Fejlede \n" + ex.Message + "\n" + ex);
+            }
+            
             // Send the message to the connected TcpServer.
-            stream.Write(data, 0, data.Length); //Sender beskeden
+            stream.Write(encryptedData, 0, encryptedData.Length); //Sender beskeden
 
-            stackpanel.Children.Add(new TextBlock { Foreground = sentMessage.Foreground, FontSize = sentMessage.FontSize, Margin = sentMessage.Margin, Background = sentMessage.Background, Text = sendMessage.Text });
+            stackpanel.Children.Add(new TextBlock { Foreground = sentMessage.Foreground, FontSize = sentMessage.FontSize, Margin = sentMessage.Margin, Background = sentMessage.Background, TextWrapping = sentMessage.TextWrapping, Text = sendMessage.Text });
+            stackpanel.Children.Add(new TextBlock { Foreground = sentMessage.Foreground, FontSize = sentMessage.FontSize, Margin = sentMessage.Margin, Background = sentMessage.Background, TextWrapping = sentMessage.TextWrapping, Text = String.Join(",", encryptedData.Select(p => p.ToString())) });
+            string fileName = @"C:\Users\jeppe\Desktop\DDU Projekt\penisw.txt";
+            // Check if file already exists. If yes, delete it.
+
+            // Create a new file     
+            using (FileStream fs = File.Create(fileName))
+            {
+                // Add some text to file    
+                Byte[] title = new UTF8Encoding(true).GetBytes(String.Join(",", encryptedData.Select(p => p.ToString())));
+                fs.Write(title, 0, title.Length);
+                byte[] author = new UTF8Encoding(true).GetBytes("Mahesh Chand");
+                fs.Write(author, 0, author.Length);
+            }
+
             sendMessage.Text = "";
 
             //mre.Set(); //Starter receive threaden igen
@@ -152,6 +192,63 @@ namespace TCPclient
             
         }
 
-        
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+
+                    //Import the RSA Key information. This only needs
+                    //toinclude the public key information.
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Encrypt the passed byte array and specify OAEP padding.  
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.  
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This needs
+                    //to include the private key information.
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Decrypt the passed byte array and specify OAEP padding.  
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.  
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+        }
     }
 }
